1dea9fbc124c905b0c15eacb865e74aa29d3985d
vvill <virginie.villard@le-campus-numerique.fr>
2022-03-26 21:45:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// getting-started.js
+const mongoose = require('mongoose');
+
 app.listen(port, () => console.log('Server started : ' + port));
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/test');
+}
