42574a25033e916cbad34a58e788ab35e403d64d
vvill <virginie.villard@le-campus-numerique.fr>
2022-03-26 01:31:52 +0100

    add products-api.js

---
+++ products-api.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Where we will keep products
+let products = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+// bodyParser.urlencoded is the default body type for forms
+app.use(bodyParser.urlencoded({ extended: false }));
+// application/json is something we'd use when requesting a resource using jQuery or backend REST client
+app.use(bodyParser.json());
+
+app.post('/product', (req, res) => {
+    const product = req.body;
+
+    // Output the product to the console for debugging
+    console.log(product);
+    books.push(product);
+
+    res.send('Product is added to the database');
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
